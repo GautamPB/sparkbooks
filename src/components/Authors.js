6f137fcd2f1509eab87c47c3286e600f7bf665f9
GautamPB <pbg2402@gmail.com>
2021-02-25 22:54:59 +0530

    Added: Featured author page

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import '../styles/Authors.css'
+import BookPreview from './BookPreview'
+import db from '../firebase'
 
 const Authors = () => {
     const { author } = useParams()
 
+    const [books, setBooks] = useState([])
+
+    db.collection('books')
+        .where('author', '==', author)
+        .onSnapshot((snapshot) =>
+            setBooks(
+                snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    data: doc.data(),
+                }))
+            )
+        )
+
     return (
         <div className="authors">
-            <h1>{author}</h1>
+            <h1>Books by {author.toUpperCase()}</h1>
+
+            <div className="books">
+                {books.map((book) => (
+                    <BookPreview
+                        id={book.data.id}
+                        title={book.data.title}
+                        author={book.data.title}
+                        price={book.data.price}
+                        rating={book.data.rating}
+                        image={book.data.image}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
